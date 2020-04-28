6ccc3b41a9034ccb3319c987b6b2898cb224799f
AleDegio <50446554+AleDegio@users.noreply.github.com>
2020-04-28 09:15:50 +0200

    Update Server_ECHO.py

--- DeGiovannini/Server_ECHO.py
+++ DeGiovannini/Server_ECHO.py
@@ -8,7 +8,7 @@ ip = "127.0.0.1"
 porta = 20001
 bufferSize = 1024
 
-#Creazione del Socket Client 
+#Creazione del Socket Server 
 socketServer = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socketServer.bind((ip, porta))
 print("il signor Server la puo' ricevere")
